75c47174978388f7b1cd0c757da67b4cedf8e376
Finn <finnp@schule.koeln>
2020-01-21 19:09:05 +0100

    Klasse Planet erstellt

---
+++ EVE_Fake/EVE_Fake/Planet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVE_Fake
+{
+    class Planet
+    {
+        public string PlanetName { get; set; }
+        public List<Location> Locations { get; set; }
+
+        public Planet(string Planet, List<Location> locations)
+        {
+            PlanetName = Planet;
+            Locations = locations;
+        }
+    }
+}
